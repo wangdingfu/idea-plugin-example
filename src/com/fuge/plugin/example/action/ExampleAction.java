4b76fc671f61a0d68b4467f440fdcfe1068f4881
wangdingfu <wangdingfu1024@163.com>
2022-03-07 19:30:45 +0800

    新增对象遍历功能

--- src/com/fuge/plugin/example/action/ExampleAction.java
+++ src/com/fuge/plugin/example/action/ExampleAction.java
@@ -5,8 +5,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.apache.commons.compress.utils.Lists;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,19 +27,54 @@ public class ExampleAction extends AnAction {
                 PsiParameterList parameterList = method.getParameterList();
                 PsiParameter[] parameters = parameterList.getParameters();
                 for (PsiParameter parameter : parameters) {
-                    PsiTypeElement parameterElement = parameter.getTypeElement();
-                    PsiClass classElement = JavaPsiFacade.getInstance(parameter.getProject()).findClass(parameterElement.getType().getCanonicalText(), parameterElement.getResolveScope());
-                    if (Objects.nonNull(classElement)) {
-                        System.out.println(classElement.getText());
-                        List<PsiField> fields = getFields(classElement);
-                        for (PsiField field : fields) {
-                            String text = field.getText();
-                            System.out.println(field.getName() + "的内容为:" + text);
-                        }
-                    }
+                    PsiClass classElement = convert(parameter.getProject(), parameter.getTypeElement());
+                    List<String> strings = recursiveObject(classElement);
+                    System.out.println(strings);
                 }
             }
         }
+    }
+
+
+    private List<String> recursiveObject(PsiClass classElement) {
+        List<String> fileNameList = Lists.newArrayList();
+        if (Objects.nonNull(classElement)) {
+            String text = classElement.getText();
+            System.out.println("正在递归" + classElement.getName() + "对象.对象内容为:\r\n" + text);
+            List<PsiField> fields = getFields(classElement);
+            for (PsiField field : fields) {
+                String fieldText = field.getText();
+                System.out.println("正在解析" + field.getName() + "字段. 字段内容为:\r\n" + fieldText);
+                PsiType type = field.getType();
+                String canonicalText = type.getCanonicalText();
+                System.out.println("字段【" + field.getName() + "】的类型为:" + canonicalText);
+                switch (canonicalText) {
+                    case "java.lang.String":
+                    case "java.lang.Integer":
+                    case "java.math.BigDecimal":
+                    case "java.lang.Double":
+                    case "java.lang.Float":
+                    case "java.lang.Long":
+                    case "java.lang.Short":
+                        fileNameList.add(field.getName() + "的类型为:" + canonicalText);
+                        break;
+                    case "java.util.List":
+                        //是list集合 需要特殊处理
+                        System.out.println(field.getName() + "字段是list 暂时无法遍历");
+                        break;
+                    default:
+                        //是其他对象 则需要便利
+                        fileNameList.addAll(recursiveObject(convert(field.getProject(), field.getTypeElement())));
+                }
+            }
+        }
+        return fileNameList;
+    }
+
+
+    public PsiClass convert(Project project, PsiTypeElement psiTypeElement) {
+        return JavaPsiFacade.getInstance(project).findClass(psiTypeElement.getType().getCanonicalText(), psiTypeElement.getResolveScope());
+
     }
 
     /**
