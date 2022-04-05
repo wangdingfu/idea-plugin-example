1686d37e8a450e653736f59a49dc640606114853
wangdingfu <wangdingfu1024@163.com>
2022-04-05 23:13:38 +0800

    测试方法修改

--- src/com/fuge/plugin/example/action/ExampleAction.java
+++ src/com/fuge/plugin/example/action/ExampleAction.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
 import org.apache.commons.compress.utils.Lists;
 
 import java.util.ArrayList;
@@ -23,7 +24,13 @@ public class ExampleAction extends AnAction {
         if (Objects.nonNull(psiClass)) {
             System.out.println(psiClass.getText());
             PsiMethod[] methods = psiClass.getMethods();
+            PsiModifierList modifierList1 = psiClass.getModifierList();
+            PsiAnnotation[] annotations1 = modifierList1.getAnnotations();
+            System.out.println(annotations1.toString());
             for (PsiMethod method : methods) {
+                PsiModifierList modifierList = method.getModifierList();
+                PsiAnnotation[] annotations = modifierList.getAnnotations();
+                System.out.println(annotations.toString());
                 PsiParameterList parameterList = method.getParameterList();
                 PsiParameter[] parameters = parameterList.getParameters();
                 for (PsiParameter parameter : parameters) {
@@ -39,16 +46,30 @@ public class ExampleAction extends AnAction {
     private List<String> recursiveObject(PsiClass classElement) {
         List<String> fileNameList = Lists.newArrayList();
         if (Objects.nonNull(classElement)) {
+            String qualifiedName = classElement.getQualifiedName();
+            if(Objects.nonNull(qualifiedName) && jclx(qualifiedName)){
+                fileNameList.add(classElement.getName() + "的类型为:" + qualifiedName);
+                return fileNameList;
+            }
             String text = classElement.getText();
             System.out.println("正在递归" + classElement.getName() + "对象.对象内容为:\r\n" + text);
+
             List<PsiField> fields = getFields(classElement);
             for (PsiField field : fields) {
                 String fieldText = field.getText();
                 System.out.println("正在解析" + field.getName() + "字段. 字段内容为:\r\n" + fieldText);
                 PsiType type = field.getType();
                 String canonicalText = type.getCanonicalText();
-                System.out.println("字段【" + field.getName() + "】的类型为:" + canonicalText);
-                switch (canonicalText) {
+                PsiClass psiClass = PsiUtil.resolveClassInType(type);
+                if(Objects.isNull(psiClass)){
+                    throw new RuntimeException("psiClass is、null");
+                }
+                qualifiedName = psiClass.getQualifiedName();
+                if(Objects.isNull(qualifiedName)){
+                    throw new RuntimeException("qualifiedName is null");
+                }
+                System.out.println("字段【" + field.getName() + "】的类型为:" + qualifiedName);
+                switch (qualifiedName) {
                     case "java.lang.String":
                     case "java.lang.Integer":
                     case "java.math.BigDecimal":
@@ -56,19 +77,39 @@ public class ExampleAction extends AnAction {
                     case "java.lang.Float":
                     case "java.lang.Long":
                     case "java.lang.Short":
-                        fileNameList.add(field.getName() + "的类型为:" + canonicalText);
+                        fileNameList.add(field.getName() + "的类型为:" + qualifiedName);
                         break;
                     case "java.util.List":
                         //是list集合 需要特殊处理
                         System.out.println(field.getName() + "字段是list 暂时无法遍历");
+                        //获取Java的泛型PsiType类型
+                        PsiType iterableType = PsiUtil.extractIterableTypeParameter(type, false);
+                        //获取Type的PsiClass
+                        PsiClass iterableClass = PsiUtil.resolveClassInClassTypeOnly(iterableType);
+                        fileNameList.addAll(recursiveObject(iterableClass));
                         break;
                     default:
-                        //是其他对象 则需要便利
+                        //是其他对象 则需要遍历
                         fileNameList.addAll(recursiveObject(convert(field.getProject(), field.getTypeElement())));
                 }
             }
         }
         return fileNameList;
+    }
+
+
+    private boolean jclx(String type){
+        switch (type){
+            case "java.lang.String":
+            case "java.lang.Integer":
+            case "java.math.BigDecimal":
+            case "java.lang.Double":
+            case "java.lang.Float":
+            case "java.lang.Long":
+            case "java.lang.Short":
+                return true;
+        }
+        return false;
     }
 
 
